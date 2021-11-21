011d28b0a18cca7f6938b439fedd565352ff98e7
wincysoftwaresolutions <81380752+wincysoftwaresolutions@users.noreply.github.com>
2021-11-21 21:59:58 +0530

    Fix

--- config.js
+++ config.js
@@ -1,11 +1,12 @@
 const env = "qa";
 const config = {
   qa: {
-    DBURL: "mongodb+srv://homelane:homelane@parvaluemongo.qjnom.mongodb.net/HomeLane?retryWrites=true&w=majority",
+    DBURL: "mongodb+srv://ypersonal:ypersonal@cluster0.rzskx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
     JWT_SECRET_KEY: "uXtD3EW4wwwwwwwwwwwwwwwwertyuiooooooooDDVI02nMrEoZDky3ThM",
   },
   dev: {
-    DBURL: "mongodb://127.0.0.1:27017/Cambium",
+    DBURL: "mongodb+srv://ypersonal:ypersonal@cluster0.rzskx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+   // DBURL: "mongodb://127.0.0.1:27017/Cambium",
     JWT_SECRET_KEY: "uXtD3EW4DDVI02nMrEoZDky3ThMbgvm",
   },
 };
